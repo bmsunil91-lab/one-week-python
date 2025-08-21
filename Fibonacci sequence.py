f2170920f52ea4eb93fcc45f7ada1086cf41e709
bmsunil91-lab <bmsunil91@gmail.com>
2025-08-21 18:57:55 +0530

    Add files via upload

---
+++ Fibonacci sequence.py
@@ -0,0 +1,6 @@
+a=0
+b=1
+print("Fibonacci sequence:")
+for i in range(10):
+    print(a)
+    a,b=b,a+b
---
+++ prime.py
@@ -0,0 +1,6 @@
+print("enter the number")
+n = int(input())
+if n%2==0:
+  print("it NOT prime")
+else:
+  print("it is prime")
