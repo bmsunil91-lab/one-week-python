55f36438c1c543e26d50c1f21191f197654b4381
bmsunil91-lab <bmsunil91@gmail.com>
2025-08-21 18:55:55 +0530

    Add files via upload

---
+++ swaping.py
@@ -0,0 +1,6 @@
+a=5
+b=10
+a=a+b
+b=a-b
+a=a-b
+print("show after the swapping first is a second is b",a,b)
---
+++ variable.py
@@ -0,0 +1,6 @@
+name="sunil"
+hobby="gaming"
+age=25
+print("name:",name)
+print("hobby:",hobby)
+print("age:",age)
